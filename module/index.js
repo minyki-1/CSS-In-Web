be4a820e7107e302ad2ca44d014d82ba0109f75c
CWIN77 <cwstbp0426@gmail.com>
2022-12-09 13:56:07 +0900

    style이 두번 생기는 버그 수정 후 리팩토링

--- module/index.js
+++ module/index.js
@@ -1,47 +1,30 @@
-const ciwInfo = { id: null };
-
-const makeRandomId = () => {
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-  let id = '';
-  for (let i = 0; i < 8; i++) {
-    const randomNum = Math.floor(Math.random() * chars.length);
-    id += chars.substring(randomNum, randomNum + 1);
-  }
-  ciwInfo.id = id;
-  return id;
-}
-
 export default function cssInWeb({ url }) {
   if (!url) {
     console.error("URL argument is undefined.");
     return;
   }
-  if (ciwInfo.id) return;
   if (typeof window !== 'object') return;
   const cssStorage = JSON.parse(sessionStorage.getItem("@CssInWeb"));
-  if (cssStorage && !document.getElementById(cssStorage.id)) {
-    const { id, css } = cssStorage;
-    try {
-      const style = `<style id="@CssInWeb_${id}">${css}</style>`;
+  try {
+    if (cssStorage && !document.getElementById("@CssInWeb")) {
+      const style = `<style id="@CssInWeb">${cssStorage}</style>`;
       document.body.children[0]
         .insertAdjacentHTML("beforebegin", style);
-    } catch (err) {
-      console.error(err);
     }
-  }
+  } catch (err) { console.error(err); }
   try {
     fetch(url)
       .then(res => res.json())
       .then(result => {
-        const id = "@CssInWeb_" + makeRandomId();
-        const style = `<style id="${id}">${result}</style>`;
-        if (!cssStorage) {
-          document.body.children[0]
-            .insertAdjacentHTML("beforebegin", style);
+        let isChange = false;
+        if (!document.getElementById("@CssInWeb")) {
+          isChange = true;
+        } else if (result !== cssStorage) {
+          document.getElementById("@CssInWeb").remove();
+          isChange = true;
         }
-        sessionStorage.setItem("@CssInWeb", JSON.stringify({ css: result, id }));
+        if (isChange) document.body.children[0].insertAdjacentHTML("beforebegin", `<style id="@CssInWeb">${result}</style>`);
+        sessionStorage.setItem("@CssInWeb", JSON.stringify(result));
       });
-  } catch (err) {
-    console.error(err);
-  }
+  } catch (err) { console.error(err); }
 }
--- server/src/routes/home/ctrl.js
+++ server/src/routes/home/ctrl.js
@@ -1,5 +1,5 @@
 const getRes = (req, res) => {
-  res.status(200).json("body {background-color:blue;}");
+  res.status(200).json("body {background-color:red;}");
 };
 
 module.exports = { getRes };
